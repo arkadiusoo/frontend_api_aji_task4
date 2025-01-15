a77a9782caa2d6620fb07ced6362142d39512683
arkadiusoo <arkadiusz.mlyn@icloud.com>
2025-01-15 23:57:23 +0100

    filters works

--- shop-frontend/src/components/ProductsTable.vue
+++ shop-frontend/src/components/ProductsTable.vue
@@ -1,5 +1,37 @@
 <template>
   <div>
+    <!-- Filtry -->
+    <div class="filters mb-4">
+      <div class="form-group mx-auto filter-field">
+        <label for="nameFilter">Filter by Name</label>
+        <input
+          type="text"
+          id="nameFilter"
+          class="form-control"
+          v-model="filters.name"
+          placeholder="Enter product name"
+        />
+      </div>
+      <div class="form-group mx-auto filter-field mt-3">
+        <label for="categoryFilter">Filter by Category</label>
+        <select
+          id="categoryFilter"
+          class="form-control"
+          v-model="filters.category"
+        >
+          <option value="">All Categories</option>
+          <option
+            v-for="category in uniqueCategories"
+            :key="category"
+            :value="category"
+          >
+            {{ category }}
+          </option>
+        </select>
+      </div>
+    </div>
+
+    <!-- Tabela -->
     <table class="table table-bordered table-striped">
       <thead class="thead-dark">
         <tr>
@@ -11,7 +43,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="product in products" :key="product.id">
+        <tr v-for="product in filteredProducts" :key="product.id">
           <td>{{ product.name }}</td>
           <td>{{ product.description }}</td>
           <td>{{ product.price_unit }} USD</td>
@@ -31,10 +63,44 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      filters: {
+        name: "",
+        category: "",
+      },
+    };
+  },
+  computed: {
+    filteredProducts() {
+      return this.products.filter((product) => {
+        const matchesName = product.name
+          .toLowerCase()
+          .includes(this.filters.name.toLowerCase());
+        const matchesCategory =
+          !this.filters.category || product.category === this.filters.category;
+
+        return matchesName && matchesCategory;
+      });
+    },
+    uniqueCategories() {
+      return [...new Set(this.products.map((product) => product.category))];
+    },
+  },
 };
 </script>
 
 <style>
+.filters {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.filter-field {
+  width: 20%;
+  text-align: center;
+
 .table {
   margin-top: 20px;
 }
