b921eb569697331ebbdecf9ea68869149fdf59a5
arkadiusoo <arkadiusz.mlyn@icloud.com>
2025-01-16 18:56:46 +0100

    init

---
+++ shop-frontend/src/components/ConfirmOrderButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button class="btn btn-primary mt-4" @click="openModal">
+      Confirm Order
+    </button>
+    <!-- Użycie komponentu OrderModal -->
+    <OrderModal v-if="isModalVisible" :cart="cart" @close="closeModal" />
+  </div>
+</template>
+
+<script>
+import OrderModal from "./OrderModal.vue"; // Import komponentu OrderModal
+
+export default {
+  props: {
+    cart: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: {
+    OrderModal, // Rejestracja komponentu
+  },
+  data() {
+    return {
+      isModalVisible: false, // Kontrola widoczności modalu
+    };
+  },
+  methods: {
+    openModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
+  },
+};
+</script>
--- shop-frontend/src/views/CartView.vue
+++ shop-frontend/src/views/CartView.vue
@@ -5,22 +5,66 @@
       <p>Your cart is empty.</p>
     </div>
     <div v-else>
-      <CartTable
-        :cart="cart"
-        @updateQuantity="updateQuantity"
-        @removeFromCart="removeFromCart"
-      />
+      <table class="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in cart" :key="item.id">
+            <td>{{ item.name }}</td>
+            <td>
+              {{
+                item.quantity
+                  ? (item.price_unit * item.quantity).toFixed(2)
+                  : item.price_unit.toFixed(2)
+              }}
+              USD
+            </td>
+            <td>
+              <div class="quantity-controls">
+                <button
+                  class="btn btn-secondary btn-sm"
+                  @click="updateQuantity(item.id, item.quantity - 1)"
+                  :disabled="item.quantity <= 1"
+                >
+                  -
+                </button>
+                <input
+                  type="text"
+                  class="quantity"
+                  v-model.number="item.quantity"
+                  @change="handleQuantityChange(item)"
+                  style="width: 50px; text-align: center"
+                  onwheel="this.blur()"
+                />
+                <button
+                  class="btn btn-secondary btn-sm"
+                  @click="updateQuantity(item.id, item.quantity + 1)"
+                >
+                  +
+                </button>
+              </div>
+            </td>
+
+            <td>
+              <button class="btn btn-danger" @click="removeFromCart(item.id)">
+                Remove
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
-import CartTable from "../components/CartTable.vue";
-
 export default {
-  components: {
-    CartTable,
-  },
   data() {
     return {
       cart: [],
@@ -44,16 +88,18 @@ export default {
 
       const product = this.cart.find((item) => item.id === productId);
 
-      if (newQuantity >= 1) {
-        product.quantity = Math.floor(newQuantity);
+      if (product && newQuantity >= 1) {
+        product.quantity = Math.floor(newQuantity); // Zaokrąglij do liczby całkowitej
         localStorage.setItem(cartKey, JSON.stringify(this.cart));
       }
     },
-    removeFromCart(productId) {
-      const username = localStorage.getItem("username");
-      const cartKey = `cart_${username}`;
-      this.cart = this.cart.filter((item) => item.id !== productId);
-      localStorage.setItem(cartKey, JSON.stringify(this.cart));
+    handleQuantityChange(item) {
+      if (!item.quantity || item.quantity < 1) {
+        item.quantity = 1; // Minimalna wartość
+      } else {
+        item.quantity = Math.floor(item.quantity); // Zaokrąglij do liczby całkowitej
+      }
+      this.updateQuantity(item.id, item.quantity);
     },
   },
 };
@@ -62,5 +108,19 @@ export default {
 <style>
 .container {
   margin-top: 20px;
+}
+
+.quantity-controls button {
+  width: 32px;
+  height: 32px;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  padding: 10;
+  margin: 0 10px;
+}
+
+.quantity {
+  font-weight: bold;
 }
 </style>
