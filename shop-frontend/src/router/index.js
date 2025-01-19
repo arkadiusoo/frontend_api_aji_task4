0253c95037386c109b59fa43e8869d2c4821cb74
arkadiusoo <arkadiusz.mlyn@icloud.com>
2025-01-20 00:30:46 +0100

    about view removed

--- shop-frontend/src/router/index.js
+++ shop-frontend/src/router/index.js
@@ -7,12 +7,7 @@ import OrdersView from "../views/OrdersView.vue";
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
-  {
-    path: "/about",
-    name: "about",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
+
   { path: "/login", name: "login", component: LoginView },
   {
     path: "/products",
