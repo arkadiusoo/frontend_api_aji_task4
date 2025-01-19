a58b1096acc79dd3812c55d6cbe5b06eed61a47f
arkadiusoo <arkadiusz.mlyn@icloud.com>
2025-01-19 23:37:46 +0100

    vue.config.js fix

--- shop-frontend/vue.config.js
+++ shop-frontend/vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
+const webpack = require("webpack");
+
+module.exports = {
   transpileDependencies: true,
-});
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: JSON.stringify(true),
+        __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+        __VUE_SSR__: JSON.stringify(false),
+        __VUE_I18N_FULL_INSTALL__: JSON.stringify(false),
+        __VUE_I18N_LEGACY_API__: JSON.stringify(false),
+        __VUE_FULL_INSTALL__: JSON.stringify(true),
+      }),
+    ],
+  },
+};
